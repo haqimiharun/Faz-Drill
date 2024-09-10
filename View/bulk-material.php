b8911fd8b0595809217f5a0ccca68a52eee8837e
Wan21gift <hazwan.mhiruddin@gmail.com>
2024-09-10 11:20:07 +0800

    Hello

--- View/bulk-material.php
+++ View/bulk-material.php
@@ -5,39 +5,39 @@
                 <h2>Bulk</h2>
                 <form action="#" method="post">
                     <div class="form-group">
-                        <label for="barite">Barite,</label>
+                        <label for="barite">Barite, Weight</label>
                         <input type="text" id="barite" name="barite" placeholder="Enter Barite">
                     </div>
                     <div class="form-group">
-                        <label for="bentonite">Bentonite</label>
+                        <label for="bentonite">Bentonite, Weight</label>
                         <input type="text" id="bentonite" name="bentonite" placeholder="Enter Bentonite">
                     </div>
                     <div class="form-group">
-                        <label for="sand">Sand</label>
+                        <label for="sand">Sand, Weight</label>
                         <input type="text" id="sand" name="sand" placeholder="Enter Sand">
                     </div>
                     <div class="form-group">
-                        <label for="lime">Lime</label>
+                        <label for="lime">Lime, Weight</label>
                         <input type="text" id="lime" name="lime" placeholder="Enter Lime">
                     </div>
                     <div class="form-group">
-                        <label for="lcm">Lost Circulation Material (LCM)</label>
+                        <label for="lcm">Lost Circulation Material (LCM), Volume</label>
                         <input type="text" id="lcm" name="lcm" placeholder="Enter Lost Circulation Material (LCM)">
                     </div>
                     <div class="form-group">
-                        <label for="cement">Cement</label>
+                        <label for="cement">Cement, Volume</label>
                         <input type="text" id="cement" name="cement" placeholder="Enter Cement">
                     </div>
                     <div class="form-group">
-                        <label for="cal_carbo">Calcium Carbonate</label>
+                        <label for="cal_carbo">Calcium Carbonate, Weight</label>
                         <input type="text" id="cal_carbo" name="cal_carbo" placeholder="Enter Calcium Carbonate">
                     </div>
                     <div class="form-group">
-                        <label for="silica_flour">Silica Flour</label>
+                        <label for="silica_flour">Silica Flour, Weight</label>
                         <input type="text" id="silica_flour" name="silica_flour" placeholder="Enter Silica Flour">
                     </div>
                     <div class="form-group">
-                        <label for="fib_mat">Fibrous Materials</label>
+                        <label for="fib_mat">Fibrous Materials, Weight</label>
                         <input type="text" id="fib_mat" name="fib_mat" placeholder="Enter Fibrous Materials">
                     </div>
                 </form>
@@ -47,23 +47,23 @@
             <h2>Liquid</h2>
                 <form>
                     <div class="form-group">
-                        <label for="drill_mud">Drilling Mud</label>
+                        <label for="drill_mud">Drilling Mud, Volume</label>
                         <input type="text" id="drill_mud" name="drill_mud" placeholder="Enter Drilling Mud">
                     </div>
                     <div class="form-group">
-                        <label for="water">Water</label>
+                        <label for="water">Water, Volume</label>
                         <input type="text" id="water" name="water" placeholder="Enter Water">
                     </div>
                     <div class="form-group">
-                        <label for="diesel">Diesel</label>
+                        <label for="diesel">Diesel, Volume</label>
                         <input type="text" id="diesel" name="diesel" placeholder="Enter Diesel">
                     </div>
                     <div class="form-group">
-                        <label for="base_oil">Base Oil</label>
+                        <label for="base_oil">Base Oil, Volume</label>
                         <input type="text" id="base_oil" name="base_oil" placeholder="Enter Base Oil">
                     </div>
                     <div class="form-group">
-                        <label for="chem_add">Chemical Additives</label>
+                        <label for="chem_add">Chemical Additives, Volume</label>
                         <input type="text" id="chem_add" name="chem_add" placeholder="Enter Chemical Additives">
                     </div>
                 </form>
