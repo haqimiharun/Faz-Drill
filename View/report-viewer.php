d72c5f54d2bdc204c7f4231d5fcf598eea4617f3
haqimiharun <haqimi.2rk@gmail.com>
2024-09-18 18:25:30 +0800

    18/9 tutup

--- View/report-viewer.php
+++ View/report-viewer.php
@@ -9,31 +9,209 @@
 <body>
     <div class="report-container">
         <header>
-            <h1>Daily Drilling Report</h1>
-            <h2>PR20140005</h2>
-            <p>Date: [Date Placeholder]</p>
+            <h3>Daily Drilling Report</h3>
         </header>
 
         <section class="section">
-            <h3>General Information</h3>
-            <table>
-                <tr>
-                    <th>Rig Name</th>
-                    <td>[Rig Name]</td>
-                </tr>
-                <tr>
-                    <th>Well Name</th>
-                    <td>[Well Name]</td>
-                </tr>
-                <!-- Add more rows as needed -->
-            </table>
-        </section>
+            <div id="well_Info">
+                <div class="well">
+                    Well: 
+                </div>
+                <div class="wellbore">
+                    Wellbore:
+                </div>
+                <div class="reportNo">
+                    Report No:
+                </div>
+                <div class="reportDate">
+                    Report Date:
+                </div>
+                <div class="wellTitle">
+                    WELL INFO
+                </div>
+                <div class="eventDesc">
+                Event Description:
+                </div>
+                <div class="waterDepth">
+                Water Depth: 
+                </div>
+                <div class="region">
+                Region:
+                </div>
+                <div class="rigName">
+                Rig Name:
+                </div>
+            </div>
+            <div class="sixItem">
+                <div class="Obj">
+                Objection
+                </div>
+                <div class="field-platform">
+                Field / Platform
+                </div>
+                <div class="AFE_No">
+                AFE No
+                </div>
+                <div class="Start_date">
+                Start date
+                </div>
+                <div class="Spud_date">
+                Spud date
+                </div>
+                <div class="End_date">
+                End date
+                </div>
+                <div class="blnk">:</div>
+                <div class="blnk">:</div>
+                <div class="blnk">:</div>
+                <div class="blnk">:</div>
+                <div class="blnk">:</div>
+                <div class="blnk">:</div>
 
-        <section class="section">
-            <h3>Operations Summary</h3>
-            <p>
-                [Summary of operations goes here. Placeholder for text]
-            </p>
+            </div>    
+            <div id="well_Info">    
+                <div class="block">
+                Block:
+                </div>
+                <div class="leadDS">
+                Lead DS:
+                </div>
+                <div class="NightDS">
+                Night DS:
+                </div>
+                <div class="rigName">
+                PCSB Engineer:
+                </div>
+            </div>
+            <div id="well_Info2">
+                <div class="depthDays">
+                    DEPTH DAYS
+                </div>
+                <div class="Cost">
+                    COSTS (USD)
+                </div>
+                <div class="dol">
+                    DOL :
+                </div>
+                <div class="md">
+                    MD :
+                </div>
+                <div class="rttingHrs">
+                    Rotating Hrs :
+                </div>
+                <div class="lastCasing">
+                    Last Casing :
+                </div>
+                <div class="dailyCost">
+                    Daily Cost :
+                </div>
+                <div class="dfs">
+                    DFS :
+                </div>
+                <div class="tvd">
+                    TVD :
+                </div>
+                <div class="cumRotHrs">
+                    Cum Rot Hrs :
+                </div>
+                <div class="lastHoleSize">
+                    Last Hole Size :
+                </div>
+                <div class="cummCost">
+                    Cumm Cost :
+                </div>
+                <div class="ttlDays">
+                    Total Days :
+                </div>
+                <div class="progress">
+                    Progress :
+                </div>
+                <div class="avgROP">
+                    Avg ROP :
+                </div>
+                <div class="lastShoeTMD">
+                    Last Shoe TMD :
+                </div>
+                <div class="AFECost">
+                    AFE Cost :
+                </div>
+                <div class="estDays">
+                    Est days :
+                </div>
+                <div class="finalTMD">
+                    Final TMD :
+                </div>
+                <div class="blnk1">
+                </div>
+                <div class="lastShoeTVD">
+                    Last Shoe TVD :
+                </div>
+                <div class="suppCost">
+                    Supp Costs / Days :
+                </div>
+                <div class="dailyNPT">
+                    Daily NPT :
+                </div>
+                <div class="cummNPT">
+                    Cumm NPT :
+                </div>
+                <div class="blnk2">
+                    
+                </div>
+                <div class="curnHoleSize">
+                    Current Hole Size :
+                </div>
+                <div class="expenditure">
+                    Expenditure :
+                </div>
+            </div>
+            <div class="wellTitle">
+                STATUS
+            </div>
+            <div id="well_Info3">
+                <div class="statusTitle">
+                    <div class="currStatus">
+                        Current Status
+                    </div>
+                    <div class="summary">
+                        24hr Summary
+                    </div>
+                    <div class="forecast">
+                        24hr Forecast
+                    </div>
+                    <div class="cident">
+                        Incident/Accident
+                    </div>
+                    <div class="remarks">
+                        Remarks
+                    </div>
+                </div>
+                <div class="statusInfo">
+                    <div class="currStatus">
+                        :
+                    </div>
+                    <div class="summary">
+                        :
+                    </div>
+                    <div class="forecast">
+                        :
+                    </div>
+                    <div class="cident">
+                        :
+                    </div>
+                    <div class="remarks">
+                        :
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section>
+            <div class="operationSum">
+                OPERATION SUMMARY
+            </div>
+            <div class="">
+
+            </div>
         </section>
 
         <section class="section">
