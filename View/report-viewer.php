6fb17a38213ef19893e3708728e4305da0e221e9
haqimiharun <haqimi.2rk@gmail.com>
2024-09-23 12:27:16 +0800

    half 23/9

--- View/report-viewer.php
+++ View/report-viewer.php
@@ -15,6 +15,7 @@
             box-sizing: border-box; /* Ensure padding is included in the size */
             background-color: #fff;
             border: 1px solid #000; /* Optional border for visual reference */
+            margin-bottom: 10mm; 
         }
 
         /* Prevent page from being scaled */
@@ -281,32 +282,32 @@
             <div class="operationSumPart2">
                 <div class="p3">
                     <div class="fromTo">
-                        na
+                        
                     </div> 
                     <div class="hours">
-                        na
+                        
                     </div> 
                     <div class="phaseCode">
-                        na
+                        
                     </div> 
                     <div class="actCode">
-                        na
+                        
                     </div>
                     <div class="ductiveCode">
-                        na
+                        
                     </div> 
                     <div class="NPT">
-                        na
+                        
                     </div>
                     <div class="rigStatus">
-                        na
+                    
                     </div>
                     <div class="MDForm">
-                        na
+                        
                     </div>
                 </div>
                 <div class="p4">
-                    na
+                    
                 </div>
             </div>
         </section>
@@ -338,15 +339,111 @@
                     </div>
                     <div class="bitData1" > Bit No.
                     </div>
-                    <div class="bitData2" > na
+                    <div class="bitData2" > 
                     </div>
-                    <div class="bitData3" > na
+                    <div class="bitData3" > 
                     </div>
                     <div class="bitData1" > Run
                     </div>
-                    <div class="bitData2" > na
+                    <div class="bitData2" > 
                     </div>
-                    <div class="bitData3" > na
+                    <div class="bitData3" > 
+                    </div>
+                    <div class="bitData1" > Size
+                    </div>
+                    <div class="bitData2" > 
+                    </div>
+                    <div class="bitData3" > 
+                    </div>
+                    <div class="bitData1" > Manufacturer
+                    </div>
+                    <div class="bitData2" > 
+                    </div>
+                    <div class="bitData3" > 
+                    </div>
+                    <div class="bitData1" > Type
+                    </div>
+                    <div class="bitData2" > 
+                    </div>
+                    <div class="bitData3" > 
+                    </div>
+                    <div class="bitData1" > Serial No.
+                    </div>
+                    <div class="bitData2" > 
+                    </div>
+                    <div class="bitData3" > 
+                    </div>
+                    <div class="bitData1" > TFA
+                    </div>
+                    <div class="bitData2" > 
+                    </div>
+                    <div class="bitData3" > 
+                    </div>
+                    <div class="bitData1" > Cond (IODL)
+                    </div>
+                    <div class="bitData2" > 
+                    </div>
+                    <div class="bitData3" > 
+                    </div>
+                    <div class="bitData1" > Cond (BGOR)
+                    </div>
+                    <div class="bitData2" > 
+                    </div>
+                    <div class="bitData3" > 
+                    </div>
+                    <div class="bitData1" > Jets
+                    </div>
+                    <div class="bitData2" > 
+                    </div>
+                    <div class="bitData3" > 
+                    </div>
+                    <div class="bitData1" > TMD In
+                    </div>
+                    <div class="bitData2" > 
+                    </div>
+                    <div class="bitData3" > 
+                    </div>
+                    <div class="bitData1" > TMD Out
+                    </div>
+                    <div class="bitData2" > 
+                    </div>
+                    <div class="bitData3" > 
+                    </div>
+                    <div class="bitData1" > Cum TMD
+                    </div>
+                    <div class="bitData2" > 
+                    </div>
+                    <div class="bitData3" > 
+                    </div>
+                    <div class="bitData1" > Cum Hrs
+                    </div>
+                    <div class="bitData2" > 
+                    </div>
+                    <div class="bitData3" > 
+                    </div>
+                    <div class="bitData1" > WOB Min
+                    </div>
+                    <div class="bitData2" > 
+                    </div>
+                    <div class="bitData3" > 
+                    </div>
+                    <div class="bitData1" > WOB Max
+                    </div>
+                    <div class="bitData2" > 
+                    </div>
+                    <div class="bitData3" > 
+                    </div>
+                    <div class="bitData1" > RPM Min
+                    </div>
+                    <div class="bitData2" > 
+                    </div>
+                    <div class="bitData3" > 
+                    </div>
+                    <div class="bitData1" > RPM Max
+                    </div>
+                    <div class="bitData2" > 
+                    </div>
+                    <div class="bitData3" > 
                     </div>
                 </div>
                 <div class="mudCheck1">
@@ -356,9 +453,99 @@
                     </div>
                     <div class="mudCheck1_1"> Type
                     </div>
-                    <div class="mudCheck1_2"> Synthetic Based
+                    <div class="mudCheck1_2"> <br>
                     </div>
-                    <div class="mudCheck1_3"> na
+                    <div class="mudCheck1_3"> <br>
+                    </div>
+                    <div class="mudCheck1_1"> Time/Loc
+                    </div>
+                    <div class="mudCheck1_2"> <br>
+                    </div>
+                    <div class="mudCheck1_3"> <br>
+                    </div>
+                    <div class="mudCheck1_1"> TMD (m)
+                    </div>
+                    <div class="mudCheck1_2"> <br>
+                    </div>
+                    <div class="mudCheck1_3"> <br>
+                    </div>
+                    <div class="mudCheck1_1"> Temp (°F)
+                    </div>
+                    <div class="mudCheck1_2"> <br>
+                    </div>
+                    <div class="mudCheck1_3"> <br>
+                    </div>
+                    <div class="mudCheck1_1"> Density (ppg)
+                    </div>
+                    <div class="mudCheck1_2"> <br>
+                    </div>
+                    <div class="mudCheck1_3"> <br>
+                    </div>
+                    <div class="mudCheck1_1"> ECD (ppg)
+                    </div>
+                    <div class="mudCheck1_2"> <br>
+                    </div>
+                    <div class="mudCheck1_3"> <br>
+                    </div>
+                    <div class="mudCheck1_1"> Viscosity (s/qt)
+                    </div>
+                    <div class="mudCheck1_2"> <br>
+                    </div>
+                    <div class="mudCheck1_3"> <br>
+                    </div>
+                    <div class="mudCheck1_1"> PV (cp)
+                    </div>
+                    <div class="mudCheck1_2"> <br>
+                    </div>
+                    <div class="mudCheck1_3"> <br>
+                    </div>
+                    <div class="mudCheck1_1"> YP (lbf/100ft²)
+                    </div>
+                    <div class="mudCheck1_2"> <br>
+                    </div>
+                    <div class="mudCheck1_3"> <br>
+                    </div>
+                    <div class="mudCheck1_1"> Gels (lbf/100ft²)
+                    </div>
+                    <div class="mudCheck1_2"> <br>
+                    </div>
+                    <div class="mudCheck1_3"> <br>
+                    </div>
+                    <div class="mudCheck1_1"> Api WL (cc/30min)
+                    </div>
+                    <div class="mudCheck1_2"> <br>
+                    </div>
+                    <div class="mudCheck1_3"> <br>
+                    </div>
+                    <div class="mudCheck1_1"> HTHP WL (cc/30min)
+                    </div>
+                    <div class="mudCheck1_2"> <br>
+                    </div>
+                    <div class="mudCheck1_3"> <br>
+                    </div>
+                    <div class="mudCheck1_1"> Api FC (32nd")
+                    </div>
+                    <div class="mudCheck1_2"> <br>
+                    </div>
+                    <div class="mudCheck1_3"> <br>
+                    </div>
+                    <div class="mudCheck1_1"> MBT (lbm/bbl)
+                    </div>
+                    <div class="mudCheck1_2"> <br>
+                    </div>
+                    <div class="mudCheck1_3"> <br>
+                    </div>
+                    <div class="mudCheck1_1"> Lime (lbm/bbl)
+                    </div>
+                    <div class="mudCheck1_2"> <br>
+                    </div>
+                    <div class="mudCheck1_3"> <br>
+                    </div>
+                    <div class="mudCheck1_1"> pH
+                    </div>
+                    <div class="mudCheck1_2"> <br>
+                    </div>
+                    <div class="mudCheck1_3"> <br>
                     </div>
                 </div>
                 <div class="mudCheck2">
@@ -368,10 +555,181 @@
                     </div>
                     <div class="mudCheck2_1"> Pm (cc)
                     </div>
-                    <div class="mudCheck2_2"> 0.00
+                    <div class="mudCheck2_2"> <br>
                     </div>
-                    <div class="mudCheck2_3"> na
+                    <div class="mudCheck2_3"> <br>
                     </div>
+                    <div class="mudCheck2_1"> Pf/Mf (cc/cc)
+                    </div>
+                    <div class="mudCheck2_2"> <br>
+                    </div>
+                    <div class="mudCheck2_3"> <br>
+                    </div>
+                    <div class="mudCheck2_1"> Ca+ (ppm)
+                    </div>
+                    <div class="mudCheck2_2"> <br>
+                    </div>
+                    <div class="mudCheck2_3"> <br>
+                    </div>
+                    <div class="mudCheck2_1"> K+ (ppm)
+                    </div>
+                    <div class="mudCheck2_2"> <br>
+                    </div>
+                    <div class="mudCheck2_3"> <br>
+                    </div>
+                    <div class="mudCheck2_1"> Polymer (lbm/bbl)
+                    </div>
+                    <div class="mudCheck2_2"> <br>
+                    </div>
+                    <div class="mudCheck2_3"> <br>
+                    </div>
+                    <div class="mudCheck2_1"> CaCl2 (%)
+                    </div>
+                    <div class="mudCheck2_2"> <br>
+                    </div>
+                    <div class="mudCheck2_3"> <br>
+                    </div>
+                    <div class="mudCheck2_1"> NaCl (%)
+                    </div>
+                    <div class="mudCheck2_2"> <br>
+                    </div>
+                    <div class="mudCheck2_3"> <br>
+                    </div>
+                    <div class="mudCheck2_1"> Sand (%)
+                    </div>
+                    <div class="mudCheck2_2"> <br>
+                    </div>
+                    <div class="mudCheck2_3"> <br>
+                    </div>
+                    <div class="mudCheck2_1"> Tot. Solids (%)
+                    </div>
+                    <div class="mudCheck2_2"> <br>
+                    </div>
+                    <div class="mudCheck2_3"> <br>
+                    </div>
+                    <div class="mudCheck2_1"> % Oil (%)
+                    </div>
+                    <div class="mudCheck2_2"> <br>
+                    </div>
+                    <div class="mudCheck2_3"> <br>
+                    </div>
+                    <div class="mudCheck2_1"> % H20 (%)
+                    </div>
+                    <div class="mudCheck2_2"> <br>
+                    </div>
+                    <div class="mudCheck2_3"> <br>
+                    </div>
+                    <div class="mudCheck2_1"> Oil/H20 (%)
+                    </div>
+                    <div class="mudCheck2_2"> <br>
+                    </div>
+                    <div class="mudCheck2_3"> <br>
+                    </div>
+                    <div class="mudCheck2_1"> HGSolid (lbm/bbl)
+                    </div>
+                    <div class="mudCheck2_2"> <br>
+                    </div>
+                    <div class="mudCheck2_3"> <br>
+                    </div>
+                    <div class="mudCheck2_1"> LGSolid (lbm/bbl)
+                    </div>
+                    <div class="mudCheck2_2"> <br>
+                    </div>
+                    <div class="mudCheck2_3"> <br>
+                    </div>
+                    <div class="mudCheck2_1"> <br>
+                    </div>
+                    <div class="mudCheck2_2"> <br>
+                    </div>
+                    <div class="mudCheck2_3"> <br>
+                    </div>
+                    <div class="mudCheck2_1"> Comments
+                    </div>
+                    <div class="mudCheck2_2"> <br>
+                    </div>
+                    <div class="mudCheck2_3"> <br>
+                    </div>
+                </div>
+            </div>
+            <div class="bha_part">
+                <div class="bha_title1">
+                    BHA no.# 2
+                </div>
+                <div class="bha_title2">
+                    Bit no 2
+                </div>
+                    <div class="bha_data1">
+                        Date/Time in
+                    </div>
+                    <div class="bha_data2">
+                        Date/Time Out
+                    </div>
+                    <div class="bha_data3">
+                        Purpose:
+                    </div>
+                    <div class="bha_data1">
+                        BHA Length (m)
+                    </div>
+                    <div class="bha_data2">
+                        Min. ID
+                    </div>
+                    <div class="bha_data3">
+                        Weight (Above/Below) Jars
+                    </div>
+                </div>
+        </section>
+    </div>
+
+    
+</div>
+<div class="report-container-main">
+    <div class="report-container">
+        <header>
+            <h2>Daily Drilling Report</h2>
+        </header>
+
+        <section class="section">
+            <div id="AC">
+                <div class="ACwell">
+                    Well: 
+                </div>
+                <div class="ACwellbore">
+                    Wellbore:
+                </div>
+                <div class="ACreportNo">
+                    Report No:
+                </div>
+                <div class="ACreportDate">
+                    Report Date:
+                </div>
+                <div class="AC_Title">
+                    Assembly Components
+                </div>
+                <div class="sixItem">
+                    <div class="Obj">
+                    Objection
+                    </div>
+                    <div class="field-platform">
+                    Field / Platform
+                    </div>
+                    <div class="AFE_No">
+                    AFE No
+                    </div>
+                    <div class="Start_date">
+                    Start date
+                    </div>
+                    <div class="Spud_date">
+                    Spud date
+                    </div>
+                    <div class="End_date">
+                    End date
+                    </div>
+                    <div class="blnk">:</div>
+                    <div class="blnk">:</div>
+                    <div class="blnk">:</div>
+                    <div class="blnk">:</div>
+                    <div class="blnk">:</div>
+                    <div class="blnk">:</div>
                 </div>
             </div>
         </section>
